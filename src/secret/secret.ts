dab37eff7d096569526386113cf1087b78c5dca0
unknown <csmcc2@hotmail.com>
2025-12-08 14:05:06 +0800

    wasm fixes

--- src/secret/secret.ts
+++ src/secret/secret.ts
@@ -1,9 +1,8 @@
-import initSync, { SafeRandom } from "@qmaru/rtools-web"
+import { SafeRandom } from "@qmaru/rtools-node"
 
 export type FormatType = "base64" | "hex"
 
 export const Generate = async (length: number, format: FormatType) => {
-  await initSync()
   const safeBytes = SafeRandom.gen_bytes(length)
   if (format === "hex") {
     return safeBytes.to_hex()
